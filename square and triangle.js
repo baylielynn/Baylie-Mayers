0c3f28eb7454c3b81f435f97a3772d669003df4c
baylielynn <122933518+baylielynn@users.noreply.github.com>
2023-01-20 13:00:51 -0600

    Add files via upload

---
+++ square and triangle.js
@@ -0,0 +1,16 @@
+function setup() {
+  colorMode(HSB);
+  createCanvas(1500, 800);
+}
+
+function draw() {
+  background(95,100,200);
+
+//circle
+fill(0,0,100);
+ellipse(350,400,550,550);
+
+//square
+fill(0,0,100);
+square(850,120,550)
+}
